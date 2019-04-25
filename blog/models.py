682016f167080ebeea6b111f1159c058b5f71c88
lurenqiyue <1831545391@qq.com>
2019-04-25 12:04:19 +0800

    将Model层划分为三类：blog、config、comment，便于维护

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+class Category(models.Model):
+    STATUS_NORMAL = 1
+    STATUS_DELETE = 0
+    STATUS_ITEMS = ((STATUS_NORMAL, '正常'), (STATUS_DELETE, '删除'),)
--- typeidea/settings.py
+++ typeidea/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
+    'config.apps.ConfigConfig',
+    'comment.apps.CommentConfig',
 ]
 
 MIDDLEWARE = [
